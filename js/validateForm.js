42c766928ef1df0f2da1c7576bfe932a74354f9c
Lavina Lobo <reachlavina@gmail.com>
2021-09-29 18:25:10 +1000

    refined validation

--- js/validateForm.js
+++ js/validateForm.js
@@ -9,133 +9,99 @@ const spanError = document.getElementsByClassName("err-task");
 
 /* Declaring variables to store form field values */
 // let taskName = nameData
-let error = []
-let validationFail = 0;
+let error = [];
+let validateFlag = 0;
 
-function errorMessageGenerator(parentElement, spanElement, errMessage ){ 
-        spanElement.innerHTML = errMessage;
-        spanElement.style.color = 'red';
-        parentElement.style.border = '1px solid red';
+function errorMessageGenerator(parentElement, spanElement, errMessage) {
+  spanElement.innerHTML = errMessage;
+  spanElement.style.color = "red";
+  parentElement.style.border = "1px solid red";
 }
 
-function errorResetMessage(parentElement, spanElement, passMsg){
-        spanElement.innerHTML = passMsg
-        spanElement.style.color = 'green';
-        parentElement.style.border = '1px solid black';
+function errorResetMessage(parentElement, spanElement, passMsg) {
+  spanElement.innerHTML = passMsg;
+  spanElement.style.color = "green";
+  parentElement.style.border = "1px solid black";
 }
-
 
 /* Applying event listeners on input fields */
-taskForm.addEventListener('submit', (event) => {
-    let formData = taskForm.value
-    event.preventDefault()
-    event.stopPropagation();
-    console.log(validationFail)
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  event.stopPropagation();
 
-    //validate the task name
-    let nameData = taskName.value
+  //validate the task name
+  if (taskName.value.length < 5) {
+    spanElement = spanError[1];
+    errMessage = " Must contain atleast 5 letters";
+    errorMessageGenerator(taskName, spanElement, errMessage);
+  } else {
+    spanElement = spanError[1];
+    passMsg = " Go to next point";
+    errorResetMessage(taskName, spanElement, passMsg);
+    validateFlag++;
+  }
 
-    if (nameData.length < 5 ){
-        spanElement = spanError[1];
-        errMessage = ' Must contain atleast 5 letters'
-        errorMessageGenerator(taskName, spanElement, errMessage);
-    }else{
-        spanElement = spanError[1];
-        passMsg = ' Go to next point'
-        errorResetMessage(taskName, spanElement, passMsg);
-        validationFail ++
-    }
+  // validate assigned To
+  if (taskAssignedTo.value.length < 5) {
+    spanElement = spanError[2];
+    errMsg = " Please complete this field to continue. ";
+    errorMessageGenerator(taskAssignedTo, spanElement, errMsg);
+  } else {
+    spanElement = spanError[2];
+    passMsg = " Go to next point";
+    errorResetMessage(taskAssignedTo, spanElement, passMsg);
+    validateFlag++;
+  }
 
-    if (validationFail > 0) {
-        console.log("inside")
-        validationFail = 0;
-        spanElement = spanError[0];
-        errMessage = ' Required fields should be completed'
-        errorMessageGenerator(taskForm, spanElement, errMessage);
-      }
+  // validate Description
+  if (taskDescription.value.length < 5) {
+    spanElement = spanError[3];
+    errMsg = " Description is required. ";
+    errorMessageGenerator(taskDescription, spanElement, errMsg);
+  } else {
+    spanElement = spanError[3];
+    passMsg = " Go to next point";
+    errorResetMessage(taskDescription, spanElement, passMsg);
+    validateFlag++;
+  }
+
+  //validate date
+  if (taskDate.value === "") {
+    spanElement = spanError[4];
+    errMsg = " Must choose one date";
+    errorMessageGenerator(taskDate, spanElement, errMsg);
+  } else {
+    spanElement = spanError[4];
+    passMsg = " Go to next point";
+    errorResetMessage(taskDate, spanElement, passMsg);
+    validateFlag++;
+  }
+
+  // validate status
+  if (taskStatus.value === "Select...") {
+    spanElement = spanError[5];
+    errMsg = " Choose one option";
+    errorMessageGenerator(taskStatus, spanElement, errMsg);
+  } else {
+    spanElement = spanError[5];
+    passMsg = " Good to go.";
+    errorResetMessage(taskStatus, spanElement, passMsg);
+    validateFlag++;
+  }
+  console.log(validateFlag);
+
+  if (validateFlag >= 5) {
+    validateFlag = 0;
+    errMessage = "";
+    spanElement = spanError[0];
+    errorMessageGenerator(taskForm, spanElement, errMessage);
+    // call addTask function here
+    // reset the form
+    taskForm.reset();
+  } else {
+    validateFlag = 0;
+    spanElement = spanError[0];
+    errMessage = " Required fields should be completed";
+    errorMessageGenerator(taskForm, spanElement, errMessage);
+  }
 });
-
-// taskName.addEventListener('focusout', () => {
-//      let nameData = taskName.value
-
-//     if (nameData.length < 5 ){
-//         spanElement = spanError[1];
-//         errMessage = ' Must contain atleast 5 letters'
-//         errorMessageGenerator(taskName, spanElement, errMessage);
-//     }else{
-//         spanElement = spanError[1];
-//         passMsg = ' Go to next point'
-//         errorResetMessage(taskName, spanElement, passMsg);
-//     }
-
-// })   
-// taskAssignedTo.addEventListener('focusout', () => {
-//     let assignedToData = taskAssignedTo.value
-
-//    if (assignedToData.length < 5 ){
-//        spanElement = spanError[2];
-//        errMsg = ' Please complete this field to continue. '
-//        errorMessageGenerator(taskAssignedTo, spanElement, errMsg);
-//    }else{
-//        spanElement = spanError[2];
-//        passMsg = ' Go to next point'
-//        errorResetMessage(taskAssignedTo, spanElement, passMsg);
-//    }
-
-// })   
-// taskDescription.addEventListener('focusout', () => {
-//     let taskDescriptionData = taskDescription.value
-
-//    if (taskDescriptionData.length < 5 ){
-//        spanElement = spanError[3];
-//        errMsg = ' Description is required. '
-//        errorMessageGenerator(taskDescription, spanElement, errMsg);
-//    }else{
-//        spanElement = spanError[3];
-//        passMsg = ' Go to next point'
-//        errorResetMessage(taskDescription, spanElement, passMsg);
-//    }
-
-// }) 
-// taskDate.onclick = () => {
-//     let now = new Date();
-//     let month = now.getMonth() + 1;
-//     if (month < 10) {
-//         month = "0" + month;
-//     }
-//     let currentDate = now.getFullYear() + "-" + month + "-" + now.getDate();
-//     taskDate.setAttribute("min", currentDate);
-// };  
-// taskDate.addEventListener('focusout', () => {
-//     let taskDateData = taskDate.value
-    
-//    if (!taskDateData){
-      
-//        spanElement = spanError[4];
-//        errMsg = ' Must choose one date';
-//        errorMessageGenerator(taskDate, spanElement, errMsg);
-//    }else{
-//        spanElement = spanError[4];
-//        passMsg = ' Go to next point';
-//        errorResetMessage(taskDate, spanElement, passMsg);
-//    }
-
-// })  
-// taskStatus.addEventListener('focusout', () => {
-//     let taskStatusData = taskStatus.value
-   
-//    if (taskStatusData === "Select..." ){
-//        spanElement = spanError[5];
-//        errMsg = ' Choose one option'
-//        errorMessageGenerator(taskStatus, spanElement, errMsg);
-//    }else{
-//        spanElement = spanError[5];
-//        passMsg = ' Good to go.'
-//        errorResetMessage(taskStatus, spanElement, passMsg);
-//    }
-
-// })    
-    
-    
-
-
